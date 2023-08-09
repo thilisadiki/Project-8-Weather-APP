55cc0aa1682221acbf4a43762e7058ca29393993
Thili <sadikithili@gmail.com>
2023-08-09 23:52:19 +0200

    add city name

---
+++ app.js
@@ -0,0 +1,55 @@
+ const apiKey = "fb6f00a28a35c42d2123ac8f83a62901";
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+  
+const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+
+async function getWeatherByLocation(city){
+     
+    const resp = await fetch(url(city), { mode: "cors" }); // Changed origin to mode
+
+         const respData = await resp.json();
+     
+           addWeatherToPage(respData);
+          
+     }
+
+      function addWeatherToPage(data){
+        console.log(data);
+          const temp = Ktoc(data.main.temp);
+          const cityName = data.name; // Extracting city name
+          const weather = document.createElement('div')
+          weather.classList.add('weather');
+
+          weather.innerHTML = ` <h1>${cityName}</h1>
+          <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+          <small>${data.weather[0].main}</small>`;
+
+
+        //   cleanup 
+          main.innerHTML= "";
+           main.appendChild(weather);
+      };
+
+
+     function Ktoc(K){
+         return Math.floor(K - 273.15);
+     }
+
+
+
+     form.addEventListener('submit',(e) =>{
+        e.preventDefault();
+
+        const city = search.value;
+
+        if(city){
+            getWeatherByLocation(city)
+        }
+
+     });
+
+
